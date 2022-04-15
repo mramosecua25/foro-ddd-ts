ca5e3d60f78465d38ecf9466e181daea2d930cd6
Marcelo Ramos <mramosecua@gmail.com>
2022-04-14 21:21:13 -0400

    added withchanges

---
+++ src/shared/core/WithChanges.ts
@@ -0,0 +1,21 @@
+import { Result } from './Result'
+
+export class Changes {
+  private changes: Result<any>[]
+
+  constructor () {
+    this.changes = []
+  }
+
+  public addChange (result: Result<any>): void {
+    this.changes.push(result)
+  }
+
+  public getChangeResult (): Result<any> {
+    return Result.combine(this.changes)
+  }
+}
+
+export interface WithChanges {
+  changes: Changes
+}
